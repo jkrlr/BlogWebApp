f4978d624bd80d09a3f2ca446b2d80e5cf37e645
Jitendra Kumar <jitendrar048@gmail.com>
2020-08-11 17:13:26 +0530

    Remove duplicated directory

--- BloggingApp/src/account/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import MyUser, Request
-
-# Register your models here.
-
-admin.site.register(MyUser, UserAdmin)
-admin.site.register(Request)
